ba179c2ef571020efe229a0c4f00b1d8eb25a22c
Fabio Gaiera <fagaiera@gmail.com>
2022-05-30 21:17:12 +0200

    API

--- api/main.py
+++ api/main.py
@@ -1,14 +1,25 @@
 import tensorflow as tf
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse
 
 
 app = FastAPI()
 
-MODEL = tf.keras.models.load_model("../model")
+model = tf.keras.models.load_model("../model")
 
-web_page = """<html><body>It works!</body></html>"""
+html_open_tag = """<html>"""
+html_close_tag = """</html>"""
+body_open_tag = """<body>"""
+body_close_tag = """</body>"""
 
-@app.get("/", response_class=HTMLResponse)
+
+web_page = html_open_tag + body_open_tag + """It works!""" + body_close_tag + html_close_tag
+
+@app.get("/", response_class = HTMLResponse)
 async def ping():
     return web_page
+
+@app.post("/predict", response_class = HTMLResponse)
+async def predict(file: UploadFile = File(...)):
+
+    f = await file.read()
